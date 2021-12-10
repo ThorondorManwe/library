e542e60ef7bce64c00a1badd09020ef01659a035
Carlos Rangel <charlesleimaginant@gmail.com>
2021-12-10 00:39:40 -0600

    Add basic form to get book data from the user and a function to organize data with the object constructor.

--- book.js
+++ book.js
@@ -1,3 +1,13 @@
+/* Selecciona el formulario de entrar libro y los campos */
+const formulario = document.querySelector('.formulario');
+const title = formulario.title;
+const author = formulario.author;
+const pages = formulario.pages;
+const read = formulario.read;
+const error = document.querySelector('.error');
+let myLibrary = [];                                     // Array para guardar los libros
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,12 +18,19 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(e) {
+    e.preventDefault();
     // Toma los parametros del usuario
+    const book = new Book(`${title.value}`, `${author.value}`, `${pages.value}`, `${read.value}`);
+    console.log(book.info());
 
     // Llama a la función Book (constuctor) y pasa los datos al array
+    this.reset();                                                        // Limpia el formulario una vez que captura los datos
 }
 
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 295, 'not read yet');
 
-console.log(theHobbit.info());
+formulario.addEventListener('submit', addBookToLibrary);
+
+/* console.log(theHobbit.info());
+console.log(booksList); */
